65905d894b9475eb8700794bca1c6b8d9df8f400
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2023-10-26 11:24:55 +0300

    Add jqury code

---
+++ server/data.js
@@ -0,0 +1,109 @@
+let images = [
+  {
+    author: 'Yurii',
+    alt: 'tree',
+    src: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
+  },
+  {
+    author: 'Roman',
+    alt: 'tree',
+    src: 'https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+  },
+  {
+    author: 'Taras',
+    alt: 'tree',
+    src: 'https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg',
+  },
+  {
+    author: 'Yurii',
+    alt: 'tree',
+    src: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
+  },
+  {
+    author: 'Roman',
+    alt: 'tree',
+    src: 'https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+  },
+  {
+    author: 'Taras',
+    alt: 'tree',
+    src: 'https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg',
+  },
+  {
+    author: 'Yurii',
+    alt: 'tree',
+    src: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
+  },
+  {
+    author: 'Roman',
+    alt: 'tree',
+    src: 'https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+  },
+  {
+    author: 'Taras',
+    alt: 'tree',
+    src: 'https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg',
+  },
+  {
+    author: 'Yurii',
+    alt: 'tree',
+    src: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
+  },
+  {
+    author: 'Roman',
+    alt: 'tree',
+    src: 'https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+  },
+  {
+    author: 'Taras',
+    alt: 'tree',
+    src: 'https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg',
+  },
+  {
+    author: 'Yurii',
+    alt: 'tree',
+    src: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
+  },
+  {
+    author: 'Roman',
+    alt: 'tree',
+    src: 'https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+  },
+  {
+    author: 'Taras',
+    alt: 'tree',
+    src: 'https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg',
+  },
+  {
+    author: 'Yurii',
+    alt: 'tree',
+    src: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
+  },
+  {
+    author: 'Roman',
+    alt: 'tree',
+    src: 'https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+  },
+  {
+    author: 'Taras',
+    alt: 'tree',
+    src: 'https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg',
+  },
+  {
+    author: 'Kolya',
+    alt: 'tree',
+    src: 'https://cdn.britannica.com/50/180050-138-521974A7/tree-limits-height-width-growth-ring-discussion.jpg?w=800&h=450&c=crop',
+  },
+  {
+    author: 'Ivan',
+    alt: 'tree',
+    src: 'https://d1whtlypfis84e.cloudfront.net/guides/wp-content/uploads/2019/08/03091106/Trees-1024x682.jpg',
+  },
+  {
+    author: 'Petro',
+    alt: 'tree',
+    src: 'https://cdn.britannica.com/98/214598-050-9879F2FA/giant-sequoia-tree-Sequoia-National-Park-California.jpg',
+  },
+];
+
+module.exports = images;
